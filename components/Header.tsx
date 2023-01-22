db5747f36166f60e06a16cd9e4e7ba13d42ee162
diegoarend92 <diegoaw@gmail.com>
2023-01-22 15:07:14 -0300

    feat: add header
    feat: add header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,18 @@
 import Image from 'next/image'
 import React from 'react'
+import {ChevronDownIcon, HomeIcon } from '@heroicons/react/outline'
+import {StarIcon } from '@heroicons/react/solid'
 
 function Header() {
   return (
-    <div>
-        <div>
-            <Image alt='header-image' src="https://links.papareact.com/fqy" />
+    <div className='sticky top-0 z-50 flex bg-white px-4 py-2 shadow-sm'>
+        <div className='relative h-10 w-20 flex-shrink-0 cursor-pointer'>
+            <Image src="https://seeklogo.com/images/A/anonymous-logo-7E968E8797-seeklogo.com.png" fill  style={{objectFit:"cover"}} alt='header-logo' />
+        </div>
+        <div className='mx-7 flex items-center xl:min-w-[300px]'>
+        <HomeIcon className='h-5 w-5'/>
+        <p className='ml-2 hidden flex-1 lg:inline'>Home</p>
+        <ChevronDownIcon className='h-5 w-5'/>
         </div>
     </div>
   )
--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
+  images:{ domains: ['seeklogo.com']}
 }
-
-module.exports = nextConfig
